7455e3d30374bbc87936eeb05cf160484d68f946
raymond <stancillousray@gmail.com>
2023-09-25 14:44:54 +0300

    get user's tasks info

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,9 +3,11 @@ import sys
 from requests import request
 
 user_id = sys.argv[1]
+task_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+user_info_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-tasks_response = request("GET", f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
-user_response = request("GET", f"https://jsonplaceholder.typicode.com/users/{user_id}")
+tasks_response = (request("GET", task_url))
+user_response = request("GET", user_info_url)
 
 employee_name = user_response.json().get('name')
 
@@ -14,11 +16,12 @@ total_tasks = len(tasks_response.json())
 completed_tasks_list = []
 
 for item in tasks_response.json():
-    if item.get('completed') == True:
+    if item.get('completed'):
         completed_tasks_list.append(item.get('title'))
         completed_tasks += 1
 
 
-print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks})")
+print(f"Employee {employee_name} is done \
+with tasks ({completed_tasks}/{total_tasks})")
 for task in completed_tasks_list:
     print(f"\t{task}")
