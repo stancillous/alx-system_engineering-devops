6b1dc2f39a249a37bf4c03bb862776d7952b68a6
raymond <stancillousray@gmail.com>
2023-10-10 14:38:56 +0300

    fixed pep8 errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,13 +12,12 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If subreddit is invalid, return 0.
     """
-    req_response = requests.get(
+    req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"}
     )
 
-    if req_response.status_code == 200:
-        return req_response.json().get("data").get("subscribers")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
     else:
         return 0
-
