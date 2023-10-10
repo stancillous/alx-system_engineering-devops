369b7db1c53c8423c06ad2ac3f393043464b780d
raymond <stancillousray@gmail.com>
2023-10-10 14:33:38 +0300

    top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,14 +12,14 @@ def top_ten(subreddit):
     Function that queries the an API
     - If subreddit is invalid, print None.
     """
-    response = requests.get(
+    res = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={'User-agent': 'Custom'},
         params={"limit": 10},
     )
 
-    if response.status_code == 200:
-        for get_data in response.json().get("data").get("children"):
+    if res.status_code == 200:
+        for get_data in res.json().get("data").get("children"):
             dat = get_data.get("data")
             title = dat.get("title")
             print(title)
