91ff5c9cc3c047d1f6280fd3dbb12c501f324f8e
raymond <stancillousray@gmail.com>
2023-10-11 23:31:12 +0300

    puppet manifest

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# puppet manifest that fixes WordPress's 'phpp' extentions to 'php'
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
